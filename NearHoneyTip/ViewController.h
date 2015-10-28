8023b8be3f6cdc9037fd06ebbc3acab0b6a65b77
luvgaram <garam@nhnnext.org>
2015-10-28 14:35:37 +0900

    [Ma/all] github test

---
+++ NearHoneyTip/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NearHoneyTip
+//
+//  Created by Eunjoo Im on 2015. 10. 28..
+//  Copyright © 2015년 Mamamoo-iOS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ NearHoneyTip/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  NearHoneyTip
+//
+//  Created by Eunjoo Im on 2015. 10. 28..
+//  Copyright © 2015년 Mamamoo-iOS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}

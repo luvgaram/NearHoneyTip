c52c69b18d937273910b58be749f718e43c98125
Kate KyuWon <lkwon0302@gmail.com>
2015-11-18 15:47:48 +0900

    [moo-mainTableView] customising table cell in process

---
+++ NearHoneyTip/NHTMainTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  NHTMainTableViewController.h
+//  NearHoneyTip
+//
+//  Created by Kate KyuWon on 11/18/15.
+//  Copyright © 2015 Mamamoo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NHTMainTableViewController : UITableViewController
+
+@end

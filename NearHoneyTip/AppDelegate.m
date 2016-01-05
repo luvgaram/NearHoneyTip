a4f3c490d50bce5e889ee78d79f20da3978ede6d
yunseo <yunseo@nhnnext.org>
2016-01-05 14:24:53 +0900

    [ma-sidemenu_edit]clearing NSUserDefaults

--- NearHoneyTip/AppDelegate.m
+++ NearHoneyTip/AppDelegate.m
@@ -31,8 +31,10 @@ NSMutableData *data;
     
     NSLog(@"start to set uuid");
     preferences = [NSUserDefaults standardUserDefaults];
-   // [preferences removeObjectForKey:@"UserDefault"];
+
     NSString *uidIdentifier = @"UserDefault";
+//    [preferences removeObjectForKey:uidIdentifier];
+    [[NSUserDefaults standardUserDefaults] setPersistentDomain:[NSDictionary dictionary] forName:[[NSBundle mainBundle] bundleIdentifier]];
     
     
     NSLog(@"start to get location");
